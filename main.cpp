00983d0989f0d5f0918715bee6fa5c0cbbe427aa
rez <grrkekisbest@gmail.com>
2022-01-12 11:29:34 -0500

    Made it work but want to make it so numbers are randomly placed around.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <math.h>
 #include <time.h>
+#include <vector>
+#include <string>
 
 int main() {
-    std::string Words[6]= {"Test", "Life", "Work", "God","Dog", "Hope"};
+    std::string Words[6] = {"Test", "Life", "Work", "God", "Dog", "Hope"};
+
+    /*
+    Why Am I Making An Array from 0-9 when you already have a random from 0-9 it does the same fucking thing.
     std::string Numbers[10]= {"0","1","2","3","4","5","6","7","8","9"};
+    */
+
+    std::vector<std::string> hold{};
     int Word_Amount = 0;
     int Number_Amount = 0;
 
@@ -16,25 +24,18 @@ int main() {
     std::cin >> Word_Amount;
     std::cout << "How Many Numbers do you want \n";
     std::cin >> Number_Amount;
-    //std::cout << Word_Amount << "\n" << Number_Amount << "\n";
-    int Total = Word_Amount+Number_Amount;
 
-    for(int x = 1; x <= Total; x++){
-        int Picker = (rand() % 2);
-        std::cout << " !" << Picker << "! ";
-        if (Picker == 0) {
-            for (int x = 1; x <= Number_Amount; x++) {
-                int Y = rand() % 10;
-                std::cout << Numbers[Y];
-            }
-        } else {
-            for (int x = 1; x <= Word_Amount; x++) {
-                int Y = rand() % 6;
-                std::cout << Words[Y];
-            }
-
-        }
+    for (int r = 0; r < Word_Amount; r++) {
+        int Y = rand() % 5;
+        hold.push_back(Words[Y]);
     }
-    //std::cout << "\n " << Total << "\n";
-main();
-}
+    for (int r = 0; r < Number_Amount; r++) {
+        int Y = rand() % 10;
+        hold.push_back(std::to_string(Y));
+    }
+    std::cout << "Hold size is : "<< hold.size() << "\n";
+    for(int r = 0;r < hold.size();r++){
+    std::cout << hold.at(r) /*<< ' '*/;
+    }
+    }
+
