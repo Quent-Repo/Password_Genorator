ef3865c47c711420511309fe1ab4bd185822e357
rez <grrkekisbest@gmail.com>
2022-01-15 23:58:52 -0500

    push

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include <math.h>
-#include <time.h>
+#include <random>
 #include <vector>
 #include <string>
+//#include "numbers.h"
+
+std::string Words[16] = {"ranked", "tasked", "leash", "putty", "picket", "compact",
+                         "nail", "donkey", "pains", "grammar", "Terminator", "Slicer",
+                         "Ninja", "cow", "Robot", "littlegirl"};
+std::vector<std::string> hold{};
+int Word_Amount = 0;
+int Number_Amount = 0;
+std::random_device rd;
 
 int main() {
-    std::string Words[6] = {"Test", "Life", "Work", "God", "Dog", "Hope"};
-
-    /*
-    Why Am I Making An Array from 0-9 when you already have a random from 0-9 it does the same fucking thing.
-    std::string Numbers[10]= {"0","1","2","3","4","5","6","7","8","9"};
-    */
-
-    std::vector<std::string> hold{};
-    int Word_Amount = 0;
-    int Number_Amount = 0;
-
-
-    srand(time(NULL));
 
 
     std::cout << "\nHow Many words do you want this to be: \n";
@@ -26,16 +21,20 @@ int main() {
     std::cin >> Number_Amount;
 
     for (int r = 0; r < Word_Amount; r++) {
-        int Y = rand() % 5;
+        int Y = rd() % 5;
         hold.push_back(Words[Y]);
     }
     for (int r = 0; r < Number_Amount; r++) {
-        int Y = rand() % 10;
+        int Y = rd() % 10;
         hold.push_back(std::to_string(Y));
     }
     std::cout << "Hold size is : "<< hold.size() << "\n";
+//int strLen=(hold.size());
+//std::cout << strLen << " " << "\n";
     for(int r = 0;r < hold.size();r++){
-    std::cout << hold.at(r) /*<< ' '*/;
+        std::cout << hold.at(r) /*<< ' '*/;
+
     }
-    }
+    main();
+}
 
